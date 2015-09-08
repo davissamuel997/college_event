8487ab3ae218bf27483bb3f9d928fd4e13894f32
SamuelDavis <sam9@cfu.net>
2015-09-08 18:25:48 -0400

    lots of changes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  respond_to :json, :html
+
+	def welcome
+		p 'hello world'
+	end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number)
+  end
+
+  private :user_params
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.where(:first_name => 'Samuel', :last_name => 'Davis', :email => 'davissamuel@knights.ucf.edu').first_or_create(:password => 'password', :password_confirmation => 'password')
+user2 = User.where(:first_name => 'Craig', :last_name => 'Livingston', :email => 'craiglivingston@knights.ucf.edu').first_or_create(:password => 'password', :password_confirmation => 'password')
+user3 = User.where(:first_name => 'David', :last_name => 'Sweat', :email => 'dsweat@knights.ucf.edu').first_or_create(:password => 'password', :password_confirmation => 'password')
