2236be6e924e6fa3fdc31649ed99da9de7e85a85
SamuelDavis <sam9@cfu.net>
2015-11-10 22:51:13 -0500

    added org show controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,18 @@ class Event < ActiveRecord::Base
 
 	belongs_to :university
 
+	def get_params
+		{
+			event_id:      id,
+			name:          name,
+			category_id:   category_id,
+			description:   description,
+			start_time:    start_time,
+			date:          date.present? ? date.strftime('%Y-%m-%d') : nil,
+			contact_phone: contact_phone,
+			contact_email: contact_email,
+			university_id: university_id
+		}
+	end
+
 end
