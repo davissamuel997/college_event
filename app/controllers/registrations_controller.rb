08841e51a9aad685e2e776b48fa4ea71db066faf
SamuelDavis <sam9@cfu.net>
2015-11-12 14:55:43 -0500

    fixed

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  skip_before_filter :require_login, :only => [:new, :create]
+
+  respond_to :json, :html
+
+  layout 'login'
+
+  def new
+    super
+  end
+
+  def create
+    # add custom create logic here
+  end
+
+  def update
+    super
+  end
+end 
